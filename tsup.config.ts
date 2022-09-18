9b8071440b48eb3b36ca8b4b63e06a70346c3b7a
Kevin Abatan <abatan.k@gmail.com>
2022-09-18 18:49:35 +0200

    fix: updating visibility of package to public

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { solidPlugin } from 'esbuild-plugin-solid'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  name: '@carere/solux',
+  name: 'solux',
   platform: 'browser',
   entry: ['src/index.ts'],
   treeshake: true,
