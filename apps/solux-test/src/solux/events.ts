f19d948a11665fb1f2e47cfa0ee09edf3fde1b3c
Kevin Abatan <abatan.k@gmail.com>
2025-03-24 16:06:43 +0100

    chore: delete test project & add knope config

--- apps/solux-test/src/solux/events.ts
+++
@@ -1,4 +0,0 @@
-import { createEvent } from "@carere/solux";
-
-export const incrementMagic = createEvent<number>("incrementMagic");
-export const decrementMagic = createEvent<number>("decrementMagic");
